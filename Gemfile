07fb486478acc15454cb39619cadfbd91044ee6b
Dmitriy Polozov <dipol.xxl@gmail.com>
2013-05-13 10:10:15 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'sqlite3'
+gem 'ohm'
+gem 'ohm-contrib'
+gem 'jquery-rails'
+gem 'devise'
+gem 'cancan'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeftOrRight::Application.config.secret_token = 'c86a8b1524df75be0a4736fd925688d68b4e9f2c2648eb40f8c5edcf5f583bd55e880b3ca1065b9fa32fc5c3c4a9843385f5df3dbea08abc904676f77880e4e8'
